8044debc5f2c6c858fb167c7c2a61a6e2792acc9
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-01 20:46:08 -0700

    Completed carousel exercise

---
+++ src/Card.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Card from "./Card";
+
+
+it("renders without crashing", () => {
+    render(<Card />);
+});
+
+// it("it should render", function() {
+//     render(<Card />);
+// });
+
+it("matches snapshot", function() {
+    const {asFragment} = render(<Card />);
+    expect(asFragment()).toMatchSnapshot();
+});
